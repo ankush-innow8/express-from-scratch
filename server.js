04ed5c7e9f075cfd0d36c026175d92a28a2bcddb
mac1 <mac1@mac1s-iMac.local>
2021-02-09 09:37:42 +0530

    error handling

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require("express")
-const connectDB = require("./config/db")
-const auth = require("./middleware/auth")
-const privateAuth = require("./middleware/privateAuth")
+var express = require('express')
+const connectDB = require('./config/db')
+const auth = require('./middleware/auth')
+const privateAuth = require('./middleware/privateAuth')
 const indexRouter = require('./routes/index')
 require('dotenv/config')
 
 //middelware
 var app = express()
 
-app.use(express.json());
+app.use(express.json())
 app.use(privateAuth)
-connectDB();
+connectDB()
 
 //routers
 
-app.use('/api',indexRouter);
+app.use('/api', indexRouter)
 
+var PORT = process.env.PORT || 5000
 
-app.listen(5000, () => {
-    console.log("Server started!")
+app.listen(PORT, () => {
+  console.log('Server started!')
 })
